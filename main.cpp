d411cef046ab714ff59b96446cc9c4e192729021
Nikita Krenov <nikitabaram@gmail.com>
2024-06-22 17:31:30 +0300

    searching for letters in a word and outputting the number

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 
 using namespace std;
 
-int main() {
+int in_the_line(char* str, char target) {
+    int count = 0;
 
-	system("pause");
-	return 0;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == target) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    char str[] = "Hello World!";
+    char target;
+    cout << "Enter the character you are looking for: ";
+    cin >> target;
+
+    int occurrences = in_the_line(str, target);
+
+    cout << "The number of occurrences of the character '" << target << "' in the string: " << occurrences << endl;
+
+    system("pause");
+    return 0;
 }
