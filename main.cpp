d3d3cc7975a8dd1776a95a586bdb6715b7e47a1e
Nikita Krenov <nikitabaram@gmail.com>
2024-06-22 17:08:33 +0300

    inserting a letter by index

--- main.cpp
+++ main.cpp
@@ -3,8 +3,31 @@
 
 using namespace std;
 
+void inserting_a_letter_by_index(char* str, char ctrlV, int position) {
+    int len = strlen(str);
+
+    for (int i = len; i >= position; i--) {
+        str[i + 1] = str[i];
+    }
+
+    str[position] = ctrlV;
+}
+
 int main() {
+    char str[] = "Hello, World!";
+    char ctrlV;
+	cout << "Enter the character to insert: ";
+	cin >> ctrlV;
+    int position;
+	cout << "Enter the position to insert the character: ";
+	cin >> position;
+	cout << endl;
+
+    cout << "Original string: " << str << endl;
+    inserting_a_letter_by_index(str, ctrlV, position);
+    cout << "String after insertion: " << str << endl;
+    cout << endl;
 
 	system("pause");
-	return 0;
+    return 0;
 }
