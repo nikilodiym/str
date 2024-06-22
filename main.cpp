7f409adb6bb75aeae378dac01503c0466b43eeac
Nikita Krenov <nikitabaram@gmail.com>
2024-06-22 17:01:24 +0300

    deletes the letter specified by the user

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 
 using namespace std;
 
-int main() {
+void del_op(char* str, char rem) {
+    char* read = str;
+    char* replacement = str;
 
+    while (*read) {
+        if (*read != rem) {
+            *replacement = *read;
+            replacement++;
+        }
+        read++;
+    }
+}
+
+int main() {
+    char str[] = "Hello World!";
+
+    char del_characted;
+	cout << "Enter the character you want to remove: ";
+	cin >> del_characted;
+    cout << "Original string: " << str << endl;
+    del_op(str, del_characted);
+    cout << "String after removing: " << str << endl;
 	system("pause");
 	return 0;
 }
