a9adfafc75bb8bc9d77787b7ab0d749b7833af82
Nikita Krenov <nikitabaram@gmail.com>
2024-06-22 16:52:32 +0300

    user write number/index and in the text del word on the stage

--- main.cpp
+++ main.cpp
@@ -3,8 +3,27 @@
 
 using namespace std;
 
-int main() {
+void my_function(char *str, int index) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
 
+    for (int i = index; i < length; i++) {
+        str[i] = str[i + 1];
+    }
+}
+
+
+int main() {
+    int enter_what_index_delete;
+
+	char str[] = "Hello World!";
+	cout << "Enter the index of the character to delete: ";
+	cin >> enter_what_index_delete;
+    cout << "Original string: " << str << endl;
+    my_function(str, enter_what_index_delete);
+    cout << "String after deletion: " << str << endl;
 	system("pause");
 	return 0;
 }
