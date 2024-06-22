8ce077682023b8587f8f9ace892190c59cde0178
Nikita Krenov <nikitabaram@gmail.com>
2024-06-22 17:13:30 +0300

    replacing dots with exclamation marks

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+void replacing_dots_with_exclamation_marks(char* str) {
+    int len = strlen(str);
+
+    for (int i = 0; i < len; i++) {
+        if (str[i] == '.') {
+            str[i] = '!';
+        }
+    }
+}
+
 int main() {
+    char str[] = "Hello. World.";
+    cout << "Original string: " << str << endl;
+    replacing_dots_with_exclamation_marks(str);
+    cout << "String after removing: " << str << endl;
 
 	system("pause");
 	return 0;
