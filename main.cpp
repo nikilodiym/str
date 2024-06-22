4fa51c801523da2201290ccc206ecfb1fc26c004
Nikita Krenov <nikitabaram@gmail.com>
2024-06-22 16:20:45 +0300

    palindrome or not?

--- main.cpp
+++ main.cpp
@@ -3,8 +3,38 @@
 
 using namespace std;
 
-int main() {
+bool isPalindrome(const char* str) {
+    int len = strlen(str);
+    int left = 0;
+    int right = len - 1;
 
+    while (left < right) {
+        while (!isalnum(str[left]) && left < right) {
+            left++;
+        }
+        while (!isalnum(str[right]) && left < right) {
+            right--;
+        }
+
+        if (tolower(str[left]) != tolower(str[right])) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+	char str[] = "allA";
+    if (isPalindrome(str)) {
+        cout << "This string is a palindrome" << endl;
+    }
+    else {
+        cout << "This string is not a palindrome" << endl;
+    }
 	system("pause");
 	return 0;
 }
