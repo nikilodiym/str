ad0343ab97d26b604fbc2b5aa5edecaeacb141c1
Nikita Krenov <nikitabaram@gmail.com>
2024-06-22 16:04:33 +0300

    delete all gap and tab insertion

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+//!!же оепье гюбдюммъ г опюйрхвмн╞ пнанрх!! ан б оепьнлс тюик╡ пнанрю мю спнж╡ ына аскн кецье онр╡л опюжчбюрх
+
 int main() {
+	char str[] = "The quick brown fox jumps over the lazy dog.";
+
+	for (int i = 0; str[i] != '\0'; ++i) {
+		if (str[i] == ' ') {
+			str[i] = '\n';
+			cout << endl;
+		}
+		else {
+			cout << str[i];
+		}
+	}
+
 
 	system("pause");
 	return 0;
